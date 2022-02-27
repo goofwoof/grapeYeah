c387ec7f5a70e02afb10aed7064c46378b1bd274
li.he <li.he@thoughtworks.com>
2022-02-27 21:27:13 +0800

    feat: add association  to basket

--- src/main/java/com/tencent/wxcloudrun/model/Basket.java
+++ src/main/java/com/tencent/wxcloudrun/model/Basket.java
@@ -2,12 +2,12 @@ package com.tencent.wxcloudrun.model;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.github.dreamyoung.mprelation.JoinColumn;
-import com.github.dreamyoung.mprelation.OneToOne;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-@TableName("basket")
+@TableName(value = "basket", resultMap = "BaseResultMap")
 public class Basket extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
@@ -17,8 +17,6 @@ public class Basket extends BaseEntity {
     private Integer goodId;
 
     @TableField(exist = false)
-    @OneToOne
-    @JoinColumn(name = "good", referencedColumnName = "good_id")
     private Good good;
 
     /**
