eccccab7b477ff563987f693afb943e2710d3702
li.he <li.he@thoughtworks.com>
2022-03-13 13:59:57 +0800

    fix table name

--- src/main/java/com/grape/yeah/model/Address.java
+++ src/main/java/com/grape/yeah/model/Address.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
-@TableName("basket")
+@TableName("address")
 public class Address extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
